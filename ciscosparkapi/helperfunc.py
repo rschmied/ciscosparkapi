2fd25c92156a0dc3c57fb8f968ece0211379ab8f
Ralph Schmieder <rschmied@cisco.com>
2016-08-01 12:04:26 +0200

    helper for Spark specific date isoform
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- ciscosparkapi/helperfunc.py
+++ ciscosparkapi/helperfunc.py
@@ -1,5 +1,6 @@
 """Package helper functions."""
 
+from datetime import datetime
 
 def utf8(string):
     """Return the 'string' as a UTF-8 unicode encoded string."""
@@ -8,3 +9,10 @@ def utf8(string):
         return string
     elif isinstance(string, str):
         return unicode(string, encoding='utf-8')
+
+def sparkISO8601(dt):
+    """Return the datetime as an ISO8601 formated string
+       (according to spark, it must look like 2016-07-28T13:14:35.000Z)
+    """
+    assert isinstance(dt, datetime)
+    return dt.strftime('%Y-%m-%dT%H:%M:%S.000Z')
