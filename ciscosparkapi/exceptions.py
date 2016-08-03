d4b6f7f95af5515416520c21d4ff8383061e3ee0
Ralph Schmieder <rschmied@cisco.com>
2016-08-03 15:20:47 +0200

    removed the SPARK_RESPONSE_CODES list
    not needed, imo as Spark sends the correct error messages
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- ciscosparkapi/exceptions.py
+++ ciscosparkapi/exceptions.py
@@ -1,25 +1,6 @@
 """ciscosparkapi exception classes."""
 
 
-SPARK_RESPONSE_CODES = {
-    200: "OK",
-    204: "Member deleted.",
-    400: "The request was invalid or cannot be otherwise served. An "
-         "accompanying error message will explain further.",
-    401: "Authentication credentials were missing or incorrect.",
-    403: "The request is understood, but it has been refused or access is not "
-         "allowed.",
-    404: "The URI requested is invalid or the resource requested, such as a "
-         "user, does not exist. Also returned when the requested format is "
-         "not supported by the requested method.",
-    409: "The request could not be processed because it conflicts with some "
-         "established rule of the system. For example, a person may not be "
-         "added to a room more than once.",
-    500: "Something went wrong on the server.",
-    503: "Server is overloaded with requests. Try again later."
-}
-
-
 class ciscosparkapiException(Exception):
     """Base class for all ciscosparkapi package exceptions."""
 
