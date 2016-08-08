f48ea777a31d1271b8003a88a3533016548b79aa
Ralph Schmieder <rschmied@cisco.com>
2016-08-08 09:41:40 +0200

    Squashed commit of the following:
    commit fcb8b91debb7cbf5c98822f8bdaf4a386f247745
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Mon Aug 8 09:40:40 2016 +0200
    
        some code shuffling
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit 9ceab84557e1395d363cdb7c4f972deb10acd5b9
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Mon Aug 8 09:40:06 2016 +0200
    
        introduce a SparkBaseAPI class
    
        and better copy for SparkBaseObject
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit 91aa2efe768d89c3bf427787e0cf836e594016c7
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Mon Aug 8 09:38:52 2016 +0200
    
        APIs use native Objects (where reasonable)
    
        - alternatively, ID strings are also OK
        - in messages, use a ‘before’ cursor to iterate through ALL messages until room creation
        - not all API calls consequently done… need to FIX this
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    commit e23915581d5bc1c281aa3d66ef5356baae3026f8
    Author: Ralph Schmieder <rschmied@cisco.com>
    Date:   Mon Aug 8 09:35:36 2016 +0200
    
        APIs registration with API instance
    
        subinstances (like messages, room, etc.) get the API instance as a parm, not the session. They then can use the api.session object when they need access to it. They can also acces other APIs using that object. E.g. messages can call self.api.rooms APIs
    
        Signed-off-by: Ralph Schmieder <rschmied@cisco.com>
    
    Signed-off-by: Ralph Schmieder <rschmied@cisco.com>

--- ciscosparkapi/api/people.py
+++ ciscosparkapi/api/people.py
@@ -3,8 +3,7 @@
 from collections import OrderedDict
 from ciscosparkapi.exceptions import ciscosparkapiException
 from ciscosparkapi.helperfunc import utf8, sparkISO8601
-from ciscosparkapi.restsession import RestSession
-from ciscosparkapi.api.sparkobject import SparkBaseObject
+from ciscosparkapi.api.sparkobject import SparkBaseObject, SparkBaseAPI
 from datetime import datetime
 
 
@@ -20,7 +19,6 @@ _API_ATTRS = [('Person ID (string)', basestring),
               ('display name (string)', basestring),
               ('Avatar URL (string)', basestring)
               ]
-_API_ENTRY_SUFFIX = 'people'
 
 
 class Person(SparkBaseObject):
@@ -35,15 +33,16 @@ class Person(SparkBaseObject):
         return self.displayName
 
 
-class PeopleAPI(object):
+class PeopleAPI(SparkBaseAPI):
     """Spark Messages API request wrapper."""
 
-    def __init__(self, session):
-        assert isinstance(session, RestSession)
-        super(PeopleAPI, self).__init__()
-        self.session = session
+    _API_ENTRY_SUFFIX = 'people'
 
-    def list(self, **kwargs):
+    def __init__(self, api):
+        super(PeopleAPI, self).__init__()
+        self.api = api
+
+    def list(self, email=None, name=None, **kwargs):
         """List people.
 
         List people in your organization.
@@ -54,9 +53,11 @@ class PeopleAPI(object):
         automatically and efficiently request the additional 'pages' of
         responses from Spark as needed until all responses have been exhausted.
 
-        **kwargs:
+        Args:
             email (string): list people with this email address
-            displayName (string): List people whose name starts with this string
+            name (string): List people whose name starts with this string
+
+        **kwargs:
             max (int): Limit the maximum number of persons in the response.
 
         Returns:
@@ -65,33 +66,48 @@ class PeopleAPI(object):
         Raises:
             SparkApiError: If the list request fails.
         """
+
+        if email:
+            assert isinstance(email, basestring)
+            kwargs['email'] = email
+
+        if name:
+            assert isinstance(name, basestring)
+            kwargs['displayName'] = displayName
+
         # API request - get items
         querylist = ['email', 'displayName', 'max']
-        items = self.session.get_items(_API_ENTRY_SUFFIX, querylist, **kwargs)
+        items = self.api.session.get_items(
+            self._API_ENTRY_SUFFIX, querylist, **kwargs)
         # Yield message objects created from the returned items JSON objects
         for item in items:
             yield Person(item)
 
-    def details(self, personId='me', **kwargs):
+    def details(self, person='me', **kwargs):
         """get details of a person.
 
-        If the personId is not set, return the details of the authenticated user.
+        If the person is not set, return the details of the authenticated user.
 
         Args:
-            personId(string): The ID of the user
+            person (string): The ID of the user
 
         Raises:
             SparkApiError: If the create operation fails.
 
         Returns:
-            Person object
+            Person object or None (if not found)
         """
+
+        if isinstance(person, Person):
+            personId = person.id
+        elif isinstance(person, basestring):
+            personId = person
+
         # API request
-        kwargs['personId'] = personId
         querylist = ['personId']
-        json_person_obj = self.session.get(
-            '%s/%s' % (_API_ENTRY_SUFFIX, personId), querylist, erc=[200, 404], **kwargs)
-        if self.session.last_response.status_code == 200:
+        json_person_obj = self.api.session.get(self._uri_append(
+            personId), querylist, erc=[200, 404], **kwargs)
+        if self.api.session.last_response.status_code == 200:
             # Return a Room object created from the response JSON data
             return Person(json_person_obj)
         else:
